e7cb3635617d174cbb949c9658ce7fbbd41b10cf
pmatyus <>
2021-05-05 15:35:21 +0000

    added str for the variable

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,13 @@
 def main():
     #write your code below this line
+    first = int(input('Give the first number:'))
+    second = int(input('Give the second number:'))
+    if (first > second):
+      print('Greater number is: ' + str(first))
+    elif (first < second):
+      print('Greater number is: ' + str(second))
+    else:
+      print('The numbers are equal!')
 
 if __name__ == '__main__':
     main()
